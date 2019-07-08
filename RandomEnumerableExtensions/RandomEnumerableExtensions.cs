57be275e45bfd2cc538c70f377ac727e979b4e32
Jonathan Wood <jwood@softcircuits.com>
2019-07-08 10:51:27 -0600

    Add project files.

---
+++ RandomEnumerableExtensions/RandomEnumerableExtensions.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) 2019 Jonathan Wood (www.softcircuits.com)
+// Licensed under the MIT license.
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftCircuits.RandomEnumerableExtensions
+{
+    public static class RandomEnumerableExtensions
+    {
+        /// <summary>
+        /// Returns a random element from a list, or null if the list is empty.
+        /// </summary>
+        /// <typeparam name="T">The type of object being enumerated</typeparam>
+        /// <param name="rand">An instance of a random number generator</param>
+        /// <returns>A random element from a list, or null if the list is empty</returns>
+        public static T Random<T>(this IEnumerable<T> list, Random rand)
+        {
+            if (list != null && list.Count() > 0)
+                return list.ElementAt(rand.Next(list.Count()));
+            return default;
+        }
+
+        /// <summary>
+        /// Returns a shuffled IEnumerable.
+        /// </summary>
+        /// <typeparam name="T">The type of object being enumerated</typeparam>
+        /// <returns>A shuffled shallow copy of the source items</returns>
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source) => source.Shuffle(new Random());
+
+        /// <summary>
+        /// Returns a shuffled IEnumerable.
+        /// </summary>
+        /// <typeparam name="T">The type of object being enumerated</typeparam>
+        /// <param name="rand">An instance of a random number generator</param>
+        /// <returns>A shuffled shallow copy of the source items</returns>
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source, Random rand)
+        {
+            var list = source.ToList();
+            list.Shuffle(rand);
+            return list;
+        }
+
+        /// <summary>
+        /// Shuffles an IList in place.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the list</typeparam>
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            list.Shuffle(new Random());
+        }
+
+        /// <summary>
+        /// Shuffles an IList in place.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the list</typeparam>
+        /// <param name="rand">An instance of a random number generator</param>
+        public static void Shuffle<T>(this IList<T> list, Random rand)
+        {
+            int count = list.Count;
+            while (count > 1)
+            {
+                int i = rand.Next(count--);
+                T temp = list[count];
+                list[count] = list[i];
+                list[i] = temp;
+            }
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,52 @@
+# RandomEnumerableExtensions
+
+[![NuGet version (SoftCircuits.RandomEnumerableExtensions)](https://img.shields.io/nuget/v/SoftCircuits.RandomEnumerableExtensions.svg?style=flat-square)](https://www.nuget.org/packages/SoftCircuits.RandomEnumerableExtensions/)
+
+```
+Install-Package SoftCircuits.RandomEnumerableExtensions
+```
+
+## Overview
+
+The RandomEnumerableExtensions class library defines `Random()` and `Shuffle()` methods. The methods are implemented as extension methods and so they are available as methods on any collection that implements `IEnumerable<T>` when the library is referenced and visible (via `using` statement).
+
+The `Random()` method returns one item randomly chosen from the collection. The `Shuffle()` method reorders the collection . If the collection implements `IList<T>`, the items are shuffled in place. Otherwise, the collection is copied in random order.
+
+## Examples
+
+The code below demonstrates the random extension methods. It starts by creating a list with sequential integers and displays that list. Next, it prints randomly chosen items from the list. And, finally, it shuffles the entire list and prints the shuffled list.
+
+```cs
+static void Main(string[] args)
+{
+    // Create ordered list
+    List<int> list = new List<int>();
+    for (int i = 1; i <= 10; i++)
+        list.Add(i);
+
+    // Create psuedo-random number generator
+    Random rand = new Random();
+
+    // Print original list
+    Console.WriteLine("Original List:");
+    Console.WriteLine(String.Join("\r\n", list));
+    Console.WriteLine();
+
+    // Print random items from the list
+    Console.WriteLine("Random Elements:");
+    Console.WriteLine(list.Random(rand));
+    Console.WriteLine(list.Random(rand));
+    Console.WriteLine(list.Random(rand));
+    Console.WriteLine(list.Random(rand));
+    Console.WriteLine(list.Random(rand));
+    Console.WriteLine();
+
+    // Shuffle and print list
+    list.Shuffle(rand);
+    Console.WriteLine("Shuffled List:");
+    Console.WriteLine(String.Join("\r\n", list));
+    Console.WriteLine();
+
+    Console.ReadKey();
+}
+```
